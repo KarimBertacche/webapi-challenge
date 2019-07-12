d05e15cd1d4db4766dfdc231d6b1ea48da6dee69
Karim B <mattiakarim@live.it>
2019-07-12 10:26:16 +0200

    Chore: added all CRUD methods for the project router

--- data/helpers/projects/projectRouter.js
+++ data/helpers/projects/projectRouter.js
@@ -52,22 +52,58 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const changes = req.body;
+
+        if(changes && Object.keys(changes).length) {
+            const updatedProject = await dbProjects.update(id, changes);
+
+            res.status(200).json(updatedProject);
+        } else {
+            res.status(400).json({
+                message: 'Name or description are missing from the request'
+            });
+        }
+
+    } catch(error) {
+        res.status(500).json({
+            message: 'Server error while updating project'
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await dbProjects.remove(id);
+
+        res.status(204).end();
+
+    } catch(error) {
+        res.status(500).json({
+            message: 'Server error while deleting the project'
+        }); 
+    }
+});
+
 async function validateById(req, res, next) {
     try {
         const { id } = req.params;
+        const project = await dbProjects.gets(id);
 
-        if(id) {
-            const project = await dbProjects.gets(id);
+        if(project) {
             req.project = project;
             next();
         } else {
             res.status(404).json({
-                message: 'Project Id not found'
+                message: 'Project not found, invalid Id'
             });
         }
     } catch(error) {
         res.status(500).json({
-            message: 'Server error while retrieving the actions for the projects'
+            message: 'Server error while retrieving the id for the project'
         });
     }
 };
